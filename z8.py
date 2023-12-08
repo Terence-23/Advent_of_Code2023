21206f1b7213110857cd8fe90610d720a3055064
terence-23 <jpuchalski1703@gmail.com>
2023-12-08 13:22:03 +0100

    Day 8

--- z8.py
+++ z8.py
@@ -1,7 +1,10 @@
 import itertools as it
+import operator
+from typing import List
+from math import gcd, lcm
 
 
-with open("input8.txt") as f:
+with open("test.txt") as f:
     lines = f.read().splitlines()
     seq = lines[0]
     travel_map = {}
@@ -12,10 +15,42 @@ with open("input8.txt") as f:
         travel_map[k] = travel
 
     pos = 'AAA'
-    for ind, v in enumerate(it.cycle(seq)):
+    for ind, v in enumerate(it.cycle(seq), 1):
         pos = travel_map[pos][0 if v == "L" else 1]
         if pos == 'ZZZ':
             print(ind)
             break
+         
+    
+
+
+
+with open("input8.txt") as f:
+    lines = f.read().splitlines()
+    seq = lines[0]
+    travel_map = {}
+    
+    for k,v in map(lambda s: s.split(' = '), lines[2:]):
+        travel = (v.split(', ')[0][1:], v.split(', ')[1][:-1])
+        # print(travel)
+        travel_map[k] = travel
+        
+    
+    nodes: List[str] = list(filter(lambda s: s[-1] == 'A', travel_map.keys())) 
+    times = []
+    print(nodes)
+    for pos in nodes:
+        for ind, v in enumerate(it.cycle(seq), 1):
+            pos = travel_map[pos][0 if v == "L" else 1]
+            
+            if pos[-1] == 'Z':
+                print(ind)
+                times.append(ind)
+                break
+    
+    print(times)
+    for lcm_v in it.accumulate(times, lcm):
+        print(lcm_v)
+    
          
     
